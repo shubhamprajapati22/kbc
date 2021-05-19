adac88303b4fb1f95c42b502c2b2caf0d2400de4
priyanshu singh <spriyanshu751@gmail.com>
2021-05-19 09:20:36 +0530

    2nd

--- home/views.py
+++ home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def game(request):
+    return render(request, "game.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def profile(request):
+    return render(request, "profile.html")
